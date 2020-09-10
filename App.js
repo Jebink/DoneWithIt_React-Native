2e73d00d9e4fb8eb45af2d69291fb6fe62ac2d42
JebinK <jebink7@gmail.com>
2020-09-11 01:34:47 +0530

    notification finished

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import AppNavigator from "./app/navigation/AppNavigator";
 import AuthNavigator from "./app/navigation/AuthNavigator";
 import AuthContext from "./app/auth/context";
 import authStorage from "./app/auth/storage";
+import { navigationRef } from "./app/navigation/rootNavigation";
 export default function App() {
   const [user, setUser] = useState();
   const [isReady, setIsReady] = useState(false);
@@ -18,9 +19,10 @@ export default function App() {
     return (
       <AppLoading startAsync={restoreUser} onFinish={() => setIsReady(true)} />
     );
+
   return (
     <AuthContext.Provider value={{ user, setUser }}>
-      <NavigationContainer theme={navigationTheme}>
+      <NavigationContainer ref={navigationRef} theme={navigationTheme}>
         {user ? <AppNavigator /> : <AuthNavigator />}
       </NavigationContainer>
     </AuthContext.Provider>
